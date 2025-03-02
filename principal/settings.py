83cde7a4f0e35c0613878343bb280986a28101f8
Alejandro Ponce <alejandro@Mac-mini-de-Alejandro.local>
2025-03-02 17:55:47 -0300

    prep para despliegue2

--- principal/settings.py
+++ principal/settings.py
@@ -130,4 +130,6 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CORS_ALLOW_ALL_ORIGINS = True
-
+CSRF_TRUSTED_ORIGINS = [
+    "web-production-eb62.up.railway.app",  # Reemplaza con el dominio de tu app
+]
