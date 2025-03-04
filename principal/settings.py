420626afa3f30e832cfc31f6d8bfcad5b64e01d7
Alejandro Ponce <alejandro@Mac-mini-de-Alejandro.local>
2025-03-04 09:29:45 -0300

    prepa para despliegue

--- principal/settings.py
+++ principal/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import dj_database_url
+from dotenv import load_dotenv
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -79,12 +82,10 @@ WSGI_APPLICATION = 'principal.wsgi.application'
 
 DATABASES = {
     'default': {
-       'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'bd_estacionamiento',
-        'USER': 'alejandro',
-        'PASSWORD': '31995655',
-        'HOST': 'localhost',
-        'PORT': '5432',
+       'default': dj_database_url.config(default=os.getenv('DATABASE_URL')),
+       'OPTIONS': {
+            'adapter': 'psycopg'
+        }
     }
 }
 
